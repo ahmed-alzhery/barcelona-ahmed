557b13562b588a70531bfaa2155cb7e11b0c4767
ahmed-alzhery <ahmad.alzhyry@gmail.com>
2026-03-30 22:29:33 +0300

    Add HeroSection component

---
+++ frontend/src/components/HeroSection.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './HeroSection.css';
+
+const HeroSection = () => {
+    return (
+        <div className="hero">
+            <div className="hero-content">
+                <h2>Welcome to Barcelona FC</h2>
+                <p>Mes que un club</p>
+                <button>Explore</button>
+            </div>
+        </div>
+    );
+};
+
+export default HeroSection;
